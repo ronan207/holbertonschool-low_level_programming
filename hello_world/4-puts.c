d905c70a24dc011c61692f1b751406550c52094f
ronan207 <12341@holbertonstudents.com>
2025-10-21 17:24:22 +0200

    i have forgoten my space2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * puts - print "Programming is like building a multilingual puzzle" 
+ * puts - print "Programming is like building a multilingual puzzle"
  * Return: Always 0 (Success)
  */
 int main(void)
