28cd36c29deed112c7acc5c6f7226367d10a32de
ronan207 <12341@holbertonstudents.com>
2025-10-21 17:45:53 +0200

    i have replace the puts to printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- * puts - print "with proper grammar, but the outcome is a piece of art"
+ * printf - print "with proper grammar, but the outcome is a piece of art"
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"with proper grammar, but the outcome is a piece of art,\n");
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
