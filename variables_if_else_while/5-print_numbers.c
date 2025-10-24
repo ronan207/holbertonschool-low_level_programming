43e999a818b31c05a1b9cf543172b24d480abd5f
ronan207 <12341@holbertonstudents.com>
2025-10-24 15:48:57 +0200

    added 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0 ; number <= 9 ; number++)
+	{
+		printf("%d", number);
+	}
+	putchar('\n');
+
+	return (0);
+}
