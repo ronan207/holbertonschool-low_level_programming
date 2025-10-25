d3070adc31faa5b4a12f7c941a411c2b1faa2170
ronan207 <12341@holbertonstudents.com>
2025-10-25 21:21:13 +0200

    added 0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Description: prints_putchar by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
