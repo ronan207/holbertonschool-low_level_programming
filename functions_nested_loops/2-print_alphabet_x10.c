a42764372cbba457185707d24d07f05491baa2d6
ronan207 <12341@holbertonstudents.com>
2025-10-26 22:09:41 +0100

    i have replace in my code the fonction to x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * the fonction print_alphabet
- * Always return 0 (Succes)
+ * print_alphabet_x10 - fonction print alphabet ten times
+ * void no return
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i = 0;
 
 	while (i <= 9)
 	{
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-	 _putchar(ch);
-	}
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
 	_putchar('\n');
 	i++;
 	}
