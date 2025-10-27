b377053fb677325b2010e8e6c129bdeda0342c63
ronan207 <12341@holbertonstudents.com>
2025-10-27 10:30:39 +0100

    i have read my fonction islower to my main.h

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+ /**
+  *
+  * _islower - Print a lowercase if is 1 and print othercase if is 0
+  * Return 0 or 1
+  */
+
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
