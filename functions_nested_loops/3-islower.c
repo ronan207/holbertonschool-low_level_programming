aa9c47a3c752dd31148a853746ef485fddad1fd4
ronan207 <12341@holbertonstudents.com>
2025-10-27 10:36:52 +0100

    i have scribe my parmeter to my variable c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include"main.h"
 
- /**
-  *
-  * _islower - Print a lowercase if is 1 and print othercase if is 0
-  * Return 0 or 1
-  */
+/**
+ * _islower - Print a lowercase if is 1 and print othercase if is 0
+ * @c: is my variable
+ * Return: 0 or 1
+ */
 
 int _islower(int c)
 
