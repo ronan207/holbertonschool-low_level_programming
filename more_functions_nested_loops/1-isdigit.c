17d80b27196dc6331b130b27cdfde02ce7eed2cd
ronan207 <12341@holbertonstudents.com>
2025-10-28 14:28:43 +0100

    i have scribe Write a function that checks for a digit 0 through 9) in my file 1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - Write a function that prints the sign of a number
+ *
+ * @c: is my variable
+ * Return: 1 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9' )
+	{
+		return (1);
+	}
+	return (0);
+}
