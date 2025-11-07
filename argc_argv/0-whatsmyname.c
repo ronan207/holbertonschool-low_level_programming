c51593f04f9c0d11c0ce080855ccd03cea2152a0
ronan207 <12341@holbertonstudents.com>
2025-11-07 16:56:33 +0100

    i have scribe Write a program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Write a program that prints its name
+ *@argc : integers variable
+ *@argv: array of character
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
