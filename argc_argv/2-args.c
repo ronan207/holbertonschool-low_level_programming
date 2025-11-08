92b325ae41098dafc28bc00a4813809396755026
ronan207 <12341@holbertonstudents.com>
2025-11-08 09:40:46 +0100

    i have scrive the commentaire

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * @argc: size of argv array and number command line arguments
+ *
+ * @argv: an array of size argc
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	int i = 0;
