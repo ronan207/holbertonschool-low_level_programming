e7c5312c19cc30ca4dd9c61c3b885c5a08ffe1bc
ronan207 <12341@holbertonstudents.com>
2025-11-09 09:32:57 +0100

    i have scribe Write a function that copies memory area

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - Write a function that copies memory area.
+ * @dest: is my first pointers
+ * @src: is my second pointers
+ * @n : is numbers of array
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+unsigned int i;
+
+for (i = 0; i != n ; i++)
+
+
+src[i] = dest[i];
+
+
+return (dest);
+}
