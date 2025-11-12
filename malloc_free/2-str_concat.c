115785b5889c0a9cfe0a4df07abcbaf81c235492
ronan207 <12341@holbertonstudents.com>
2025-11-12 07:54:12 +0100

    i have scribe Write a function that concatenates two strings

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	char *new_string;
+	int len1 = 0, len2 = 0, i, j;
+
+	if (s1 == NULL)
+	s1 = "";
+
+	if (s2 == NULL)
+	s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0')
+		len2++;
+
+	new_string = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (new_string == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		new_string[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+		new_string[i + j] = s2[j];
+
+	new_string[i + j] = '\0';
+
+	return (new_string);
+}
