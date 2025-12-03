3626535b8652e0916399e8a275e0998824b22d53
ronan207 <12341@holbertonstudents.com>
2025-12-03 13:52:04 +0100

    i have corriged with betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,19 @@
-/** 
+#ifndef DOG_H
+#define DOG_H
+
+/**
  * struct dog - information about a dog
  * @name: the dog's name
  * @owner: the owner's name
  * @age: the dog's age
  */
-struct dog 
+struct dog
 {
-    char *name;
-    char *owner;
-    float age;
+char *name;
+char *owner;
+float age;
 
 };
+
+
+#endif /* DOG_H */
