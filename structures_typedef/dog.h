74281061031796013f052e60f1ff62249834f6c1
ronan207 <12341@holbertonstudents.com>
2025-12-03 13:43:43 +0100

    i have read the structure to the name the owner and the age of the dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/** 
+ * struct dog - information about a dog
+ * @name: the dog's name
+ * @owner: the owner's name
+ * @age: the dog's age
+ */
+struct dog 
+{
+    char *name;
+    char *owner;
+    float age;
+
+};
