84929543364aa46081addfc56f652d3459a30a7d
ronan207 <12341@holbertonstudents.com>
2025-12-06 16:09:41 +0100

    i have read the typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,14 +7,16 @@
  * @owner: the owner's name
  * @age: the dog's age
  */
-struct dog
+typedef struct dog
 {
 char *name;
 char *owner;
 float age;
 
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 
 #endif /* DOG_H */
